c3b287950f2ee442f8a4bee0a85a2abdea5974b2
ViveLiberte <sidielnikovdaniil@gmail.com>
2023-10-20 19:01:34 +0300

    revert restmodels

--- API/RestModels/User/CreateUserRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace API.RestModels.User;
-
-public sealed record CreateUserRequest
-{
-    public string FirstName { get; init; }
-    public string LastName { get; init; }
-    public string Email { get; init; }
-    public string Phone { get; init; }
-}
